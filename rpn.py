2bf7a9a1b7fa34f4e08766cd85cdfee959ad4f53
amverni <amverni@umich.edu>
2018-11-05 10:51:09 -0500

    allow for different number of arguments

--- rpn.py
+++ rpn.py
@@ -1,31 +1,45 @@
 #!/usr/bin/env python3
 import operator
+from fractions import Fraction
+
+def d2f(dec):
+    return Fraction(dec)
+
+def f2d(frac):
+    if not isinstance(frac, Fraction):
+        return frac
+    return frac.numerator / frac.denominator
 
 op = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.floordiv,
-    '^': operator.pow
+    '+': (2, operator.add),
+    '-': (2, operator.sub),
+    '*': (2, operator.mul),
+    '/': (2, operator.floordiv),
+    '^': (2, operator.pow),
+    'd2f': (1, d2f),
+    'f2d': (1, f2d)
 }
 
 def calculate(args):
     # stack for caclulator
     stack = []
     # process tokens
-    # while len(stack) > 1:
     for arg in args.split():
+        print(stack)
         token = arg
         try:
             value = int(token)
-            stack.append(value)
+            stack.append(str(value))
         except ValueError:
-            val2 = int(stack.pop())
-            val1 = int(stack.pop())
-
-            # look up functino in the operator table
-            func = op[token]
-            result = func(val1, val2)
+            # look up function in the operator table
+            numArgs, func = op[token]
+            args = []
+            for arg in range(numArgs):
+                args.append(int(stack.pop()))
+            args = args[::-1]
+            print(*args)
+            print(func)
+            result = func(*args)
             stack.append(str(result))
     return int(stack[0])
 
