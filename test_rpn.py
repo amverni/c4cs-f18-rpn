a67c2fe272f441fcdea3db1f04cf7030458914ef
amverni <amverni@umich.edu>
2018-11-05 10:15:05 -0500

    Add test for fraction/decimal conversions

--- test_rpn.py
+++ test_rpn.py
@@ -1,5 +1,6 @@
 import unittest
 import rpn
+from fractions import Fraction
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
@@ -26,3 +27,15 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('2 3 ^')
         self.assertEqual(8, result)
 
+    def test_fraction(self):
+        result = rpn.calculate('1 2 /')
+        result = rpn.calculate('d2f')
+        self.assertEqual(Fraction(0.5), result)
+        result = rpn.calculate('d2f')
+        self.assertEqual(Fraction(0.5), result)
+        result = rpn.caclulate('f2d')
+        self.assertEqual(0.5, result)
+        result = rpn.caclulate('f2d')
+        self.assertEqual(0.5, result)
+
+#    def test_repeat(self):
